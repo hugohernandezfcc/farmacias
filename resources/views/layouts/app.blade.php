2583741b51d4b73f9389fb3e3bcef375120ee8eb
yorchuaeh <yorchuaeh@gmail.com>
2020-11-28 08:57:38 -0600

    regresar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,6 @@
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
     </head>
     <body class="font-sans antialiased">
-        
-
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-dropdown')
 
