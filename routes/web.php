03e07c40727f2f077ed9f721dd6558adb9f1352b
Hugo Hernandez <hugo@doitcloud.mx>
2020-11-30 20:45:26 -0600

    s

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+use App\Http\Controllers\UsersController;
 Route::get('members', [UsersController::class, 'index']);
