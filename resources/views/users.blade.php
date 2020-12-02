20d2e349c8f5a4473304acfd19fe1693a08ac620
Hugo Hernandez <hugo@doitcloud.mx>
2020-12-01 18:57:07 -0600

    s

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -16,7 +16,7 @@
                     </div>
 
                     <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                        <div class="grid grid-cols-1 md:grid-cols-2">
+                        <div >
                             <div class="p-12">
 
                                 <table id="myTable" class="display">
