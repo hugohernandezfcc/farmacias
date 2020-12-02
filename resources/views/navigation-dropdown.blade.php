4e2d2362aaa70b4a10be8a17e06406abaafd83ca
Hugo Hernandez <hugo@doitcloud.mx>
2020-12-01 18:34:21 -0600

    s

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -17,7 +17,7 @@
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('members') }}" :active="request()->routeIs('members')">
+                    <x-jet-nav-link href="/users" >
                         {{ __('Miembros de plataforma') }}
                     </x-jet-nav-link>
                 </div>
