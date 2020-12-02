5641ba71c6dd7215ef5a40076a7b29c36f352736
Hugo Hernandez <hugo@doitcloud.mx>
2020-12-01 19:18:32 -0600

    s

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -31,6 +31,11 @@
                         {{ __('Farmacias') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="/users" >
+                        {{ __('Reportes') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
