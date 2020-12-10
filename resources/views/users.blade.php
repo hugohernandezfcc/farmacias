397e3d80c41b38b32290f95060b685130be82708
Hugo Hernandez <hugo@doitcloud.mx>
2020-12-10 15:19:09 -0600

    addinng view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,45 +3,71 @@
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Lista de miembros de plataforma') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="mt-8 text-2xl">
                         Usuarios
                     </div>
 
-                    <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                        <div >
-                            <div class="p-12">
+                    <div class="mt-6 text-gray-500">
+                        Lista:
+                    </div>
+                </div>
 
-                                <table id="myTable" class="display">
-                                    <thead>
-                                        <tr>
-                                            <th>Identificador</th>
-                                            <th>Nombre</th>
-                                            <th>Correo Electrónico</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($users as $user)
-                                         <tr>
-                                             <td>{{$user->id}}</td>
-                                             <td>{{$user->name}}</td>
-                                             <td>{{$user->email}}</td>
-                                        </tr>
-                                        @endforeach 
-                                    </tbody>
-                                </table>
+                <table id="myTable" class="display">
+                    <thead>
+                        <tr>
+                            <th>Identificador</th>
+                            <th>Nombre</th>
+                            <th>Correo Electrónico</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                         <tr>
+                             <td>{{$user->id}}</td>
+                             <td>{{$user->name}}</td>
+                             <td>{{$user->email}}</td>
+                        </tr>
+                        @endforeach 
+                    </tbody>
+                </table>
+
+                <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6 border-t border-gray-200">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://tailwindcss.com/">Ingresos por farmacia</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-500">
+                                <h1>X</h1>
                             </div>
                         </div>
                     </div>
-                    <br />
+
+                    <div class="p-6 border-t border-gray-200 md:border-l">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Egresos por farmacia</div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-sm text-gray-500">
+                                <h1>X</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+                <x-jet-welcome />
             </div>
         </div>
     </div>
@@ -52,5 +78,5 @@
             $('#myTable').DataTable();
         } );
     </script>
-
 </x-app-layout>
+
