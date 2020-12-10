213db30f2784c7a2f3c2508c5e84e5f943f44529
Hugo Hernandez <hugo@doitcloud.mx>
2020-12-10 15:10:07 -0600

    s

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,10 +1,10 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div class="mt-8 text-2xl">
-        Bienvenido 
+        Vista 360º de organización
     </div>
 
     <div class="mt-6 text-gray-500">
-        Paneles de información:
+        Detalles:
     </div>
 </div>
 
